4cc40f18bc66436c6579875a2254f20ad911cd57
alecastrovisk <alexandre.acrsf@gmail.com>
2024-12-03 12:31:18 -0300

    create ecr stack

--- bin/ecommerce_ecscdk.ts
+++ bin/ecommerce_ecscdk.ts
@@ -3,4 +3,13 @@ import { EcrStack } from "../lib/ecr-stack";
 
 const app = new cdk.App();
 
-const ecrStack = new EcrStack(app, "EcrStack", {});
+const tagsInfra = {
+  cost: "ECommerceInfra",
+};
+
+const env: cdk.Environment = {
+  account: "891377140806",
+  region: "us-east-1",
+};
+
+const ecrStack = new EcrStack(app, "EcrStack", { env, tags: tagsInfra });
